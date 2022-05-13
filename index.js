393800a96a7b9d9f79cab2d24071684bdac21cca
MarianDobra <marian.dobra@sampamind.com>
2022-05-13 15:51:16 +0300

    First routes and a router

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const messagesRouter = require("./messageRouter/messagesRouter");
 
 const app = express()
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/messages",messagesRouter);
 
 const port = process.env.PORT || 8081;
 
-app.get('/', (req, res) => {
-    res.send('Hello World again1!')
-  });
+// app.get('/', (req, res) => {
+//     res.send('Hello World again1!')
+//   });
   
   app.listen(port, () => {
     console.log(`CC app is listening on port ${port}!`)
